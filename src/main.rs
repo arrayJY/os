f98678e03cfc64a9d5f7d67d69ce336143903eac
arrayJY <ji957455952@163.com>
2021-03-05 01:41:56 +0800

    ✨ vga_buffer & println.

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,17 @@
 #![no_std]
 #![no_main]
+#![feature(exclusive_range_pattern)]
+
+mod vga_buffer;
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello World";
-
 #[no_mangle]
-pub extern "C" fn _start() -> !{
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe  {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
+pub extern "C" fn _start() -> ! {
     loop {}
 }
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
     loop {}
 }
---
+++ src/vga_buffer.rs
@@ -0,0 +1,141 @@
+use core::fmt;
+use core::ops::{Deref, DerefMut};
+use lazy_static::lazy_static;
+use spin::Mutex;
+use volatile::Volatile;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u8)]
+#[allow(dead_code)]
+pub enum Color {
+    Black = 0x0,
+    Blue = 0x1,
+    Green = 0x2,
+    Cyan = 0x3,
+    Red = 0x4,
+    Magenta = 0x5,
+    Brown = 0x6,
+    LightGray = 0x7,
+    DarkGray = 0x8,
+    LightBlue = 0x9,
+    LightGreen = 0xa,
+    LightCyan = 0xb,
+    LightRed = 0xc,
+    Pink = 0xd,
+    Yellow = 0xe,
+    White = 0xf,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(transparent)]
+pub struct ColorCode(u8);
+impl ColorCode {
+    fn new(foreground: Color, background: Color) -> Self {
+        ColorCode((background as u8) << 4 | (foreground as u8))
+    }
+}
+impl Default for ColorCode {
+    fn default() -> Self {
+        ColorCode::new(Color::White, Color::Black)
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(C)]
+struct Char {
+    ascii: u8,
+    color_code: ColorCode,
+}
+
+impl Deref for Char {
+    type Target = Char;
+    fn deref(&self) -> &Self::Target {
+        self
+    }
+}
+impl DerefMut for Char {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self
+    }
+}
+
+const BUFFER_HEIGHT: usize = 25;
+const BUFFER_WIDTH: usize = 80;
+
+#[repr(transparent)]
+pub struct Buffer {
+    characters: [[Volatile<Char>; BUFFER_WIDTH]; BUFFER_HEIGHT],
+}
+
+pub struct Writer {
+    pub column_position: usize,
+    pub row_position: usize,
+    pub color_code: ColorCode,
+    pub buffer: &'static mut Buffer,
+}
+
+impl Writer {
+    pub fn write_byte(&mut self, byte: u8) {
+        match byte {
+            b'\n' => self.new_line(),
+            byte => {
+                if self.column_position >= BUFFER_WIDTH {
+                    self.new_line();
+                }
+                let row = self.row_position;
+                let col = self.column_position;
+
+                let color_code = self.color_code;
+                self.buffer.characters[row][col].write(Char {
+                    ascii: byte,
+                    color_code,
+                });
+                self.column_position += 1;
+            }
+        }
+    }
+
+    pub fn write_string(&mut self, s: &str) {
+        s.as_bytes().iter().for_each(|&byte| match byte {
+            0x20..0x7e | b'\n' => self.write_byte(byte),
+            _ => self.write_byte(0xfe),
+        })
+    }
+    fn new_line(&mut self) {
+        self.column_position = 0;
+        self.row_position += 1;
+    }
+}
+
+impl fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.write_string(s);
+        Ok(())
+    }
+}
+
+lazy_static! {
+    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
+        column_position: 0,
+        row_position: 0,
+        color_code: ColorCode::default(),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    });
+}
+
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::vga_buffer::_print(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+}
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    use core::fmt::Write;
+    WRITER.lock().write_fmt(args).unwrap();
+}
